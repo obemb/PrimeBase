f3593072398888fd4070c1716556751e8a2731d0
obemb <peterihaza@gmail.com>
2023-08-15 13:00:50 +0100

    1

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Login from './Page/Login'
 import LoanRequest from './Page/LoanRequest'
-import RepaymentBreakdown from './Page/RepaymentBreakdown'
+import repaymentSchedule from './Page/RepaymentBreakdown'
 
 const route =  [
     { path: "/Login" ,  Component: Login},
     { path: "/" ,  Component: LoanRequest},
-    { path: "/RepaymentBreakdown" ,  Component: RepaymentBreakdown},
+    { path: "/repaymentSchedule" ,  Component: repaymentSchedule},
   ]
 
 export default route;
